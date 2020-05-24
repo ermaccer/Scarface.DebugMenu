82ebaee9664f8777121698edfc32e2e7d1eaaa28
ermaccer <40604575+ermaccer@users.noreply.github.com>
2020-05-24 22:46:52 +0200

    1.0.2 support

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -6,7 +6,17 @@ extern "C"
 {
 	__declspec(dllexport) void InitializeASI()
 	{
-		Memory::VP::Patch<char>(0x7EC9C4, 0);
-		Memory::VP::Patch<char>(0x7EC9C5, 0);
+		// a rather weird way to check version but it works, right?
+		if (*(int*)0x4884AB == 0x009550E8)
+		{
+			Memory::VP::Patch<char>(0x7EC9C4, 0);		// 1.0
+			Memory::VP::Patch<char>(0x7EC9C5, 0);
+		}
+		else
+		{
+			Memory::VP::Patch<char>(0x7C1C54, 0);		// 1.0.2
+			Memory::VP::Patch<char>(0x7C1C55, 0);
+		}
+
 	}
 }
